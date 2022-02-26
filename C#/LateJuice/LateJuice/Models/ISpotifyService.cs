31dcab7e89caa10f3d9e00d9844c16b74144b22b
trevol1 <trevoledrick@gmail.com>
2022-02-26 15:47:17 +0100

    First Javascript project

---
+++ C#/LateJuice/LateJuice/Models/ISpotifyService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LateJuice.Models;
+
+namespace LateJuice.Services
+{
+    public interface ISpotifyService
+    {
+        Task<IEnumerable<Release>> GetNewReleases(string countryCode, int limit, string accessToken);
+    }
+}
---
+++ C#/LateJuice/LateJuice/Models/SpotifyService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+using LateJuice.Models;
+
+namespace LateJuice.Services
+{
+    public class SpotifyService : ISpotifyService
+    {
+        private readonly HttpClient httpClient;
+
+        public SpotifyService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+        public async Task<IEnumerable<Release>> GetNewReleases(string countryCode, int limit, string accessToken)
+        {
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+            var response = await httpClient.GetAsync($"browse/new-releases?country={countryCode}&limit={limit}");
+
+            response.EnsureSuccessStatusCode();
+
+            using var responseStream = await response.Content.ReadAsStreamAsync();
+            var responseObject = await JsonSerializer.DeserializeAsync<GetNewReleaseResult>(responseStream);
+
+            return responseObject?.albums?.items.Select(i => new Release
+            {
+                Name = i.name,
+                Date = i.release_date,
+                ImageUrl = i.images.FirstOrDefault().url,
+                Link = i.external_urls.spotify,
+                Artists = string.Join(",", i.artists.Select(i => i.name))
+            });
+        }
+    }
+}
